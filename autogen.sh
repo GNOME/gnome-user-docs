76af9de8a6742c1057b23b3e7aa276382ff89224
Petr Kovar <pkovar@redhat.com>
2017-08-08 15:52:50 +0200

    Drop gnome-common

--- autogen.sh
+++ autogen.sh
@@ -4,19 +4,37 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-(test -f $srcdir/configure.ac \
-  && test -d $srcdir/gnome-help) || {
+olddir=$(pwd)
+
+cd $srcdir
+
+(test -f $srcdir/configure.ac && test -d $srcdir/gnome-help) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level gnome-user-docs directory"
     exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME Git"
-    exit 1
-}
+# shellcheck disable=SC2016
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
 
-REQUIRED_AUTOMAKE_VERSION=1.11
-export REQUIRED_AUTOMAKE_VERSION
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+	echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+	echo "*** If you wish to pass any to it, please specify them on the" >&2
+	echo "*** '$0' command line." >&2
+	echo "" >&2
+fi
 
-. gnome-autogen.sh
+autoreconf --verbose --force --install || exit 1
+
+cd "$olddir"
+if [ "$NOCONFIGURE" = "" ]; then
+	$srcdir/configure "$@" || exit 1
+
+	if [ "$1" = "--help" ]; then exit 0
+	else
+		echo "Now type 'make' to compile $PKG_NAME" || exit 1
+	fi
+else
+	echo "Skipping configure process."
+fi
+
